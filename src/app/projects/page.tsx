fee40a7604026cec345243d4da7d106b1f95b71a
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-06-20 21:46:35 -0400

    Implement project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Container } from "@/components/Container";
+import { Heading } from "@/components/Heading";
+import { Highlight } from "@/components/Highlight";
+import { Paragraph } from "@/components/Paragraph";
+import { Products } from "@/components/Products";
+import { Metadata } from "next";
+import Image from "next/image";
 
-const projects = () => {
+export const metadata: Metadata = {
+  title: "Projects | John Doe",
+  description:
+    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+};
+
+export default function Projects() {
   return (
-    <div>projects</div>
-  )
-}
+    <Container>
+      <span className="text-4xl">⚡</span>
+      <Heading className="font-black mb-10 text-zinc-600">
+        {" "}
+        What I&apos;ve been working on
+      </Heading>
 
-export default projects
+      <Products />
+    </Container>
+  );
+}
